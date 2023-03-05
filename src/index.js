fe905ab821ac43608ee3019c3b8dc1c407a38d92
John Dana <john@danasd.com>
2023-03-05 12:35:35 -0700

    04 20 Create rough blog-post component

--- src/index.js
+++ src/index.js
@@ -2,8 +2,20 @@ import React from 'react';
 import ReactZZZ from 'react-dom';
 
 const App = () => (
-  <div>
-    Hello World!
+  <div className="ui comments">
+    <div className="comment">
+      <a href="/" className="author">
+        Sarah
+      </a>
+      <div className="metadata">
+        <span className="date">
+          Todat at 5:00 PM
+        </span>
+      </div>
+      <div className="text">
+        It's amazing.
+      </div>
+    </div>
   </div>
 )
 
